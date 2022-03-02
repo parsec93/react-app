7d9658724e6d1d1008eebca82328c7acf5ff9332
pasrec93 <rnjs8657@naver.com>
2022-03-03 01:11:33 +0900

    인프런 _ 20220303_이벤트 만들기1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   constructor(props){
       super(props);
       this.state = {
-          mode:'read',
+          mode:'welcome',
           subject:{title:'WEB', sub:'World Wide Web!'},
           welcome:{title:'Welcome', desc:'Hello, React!!'},
           contents:[
@@ -29,24 +29,17 @@ class App extends Component {
         _desc = this.state.contents[0].desc;
       }
 
-      console.log('render', this);
     return (
         <div className="App">
-          {/*<Subject>
+          <Subject
               title={this.state.subject.title}
-              sub={this.state.subject.sub}>
-          </Subject>*/}
-            <header>
-                <h1><a href="/" onClick={function(e){
-                    console.log(e);
-                    e.preventDefault();
-                    //this.state.mode = 'welcome';
-                    this.setState({
-                        mode:'welcome'
-                    });
-                }.bind(this)}>{this.state.subject.title}</a></h1>
-                {this.state.subject.sub}
-            </header>
+              sub={this.state.subject.sub}
+              onChangePage = {function(){
+                alert('hihihi');
+                this.setState({mode : 'welcome'});
+              }.bind(this)}
+          >
+          </Subject>
           <TOC data={this.state.contents}></TOC>
           <Content title={_title} desc={_desc}></Content>
         </div>
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -1,14 +1,16 @@
 import React, {Component} from "react";
 
 class Subject extends Component {
-    render(){
+    render (){
         return (
             <header>
-                <h1><a href="/">{this.props.title}</a></h1>
+                <h1><a href="/" onClick={function(e){
+                    e.preventDefault();
+                    this.props.onChangePage();
+                }.bind(this)}>{this.props.title}</a></h1>
                 {this.props.sub}
             </header>
         );
     }
 }
-
 export default Subject;
